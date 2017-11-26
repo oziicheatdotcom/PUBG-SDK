dca1c328b41f859fa2d964d78b33c4fffff7fdf0
OziiCheat <30862429+oziicheatdotcom@users.noreply.github.com>
2017-11-27 03:48:57 +0700

    Add files via upload

---
+++ SDK/PUBG_BP_FppWeaponSlotHudWidget_classes.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+// PlayerUnknown's Battlegrounds (2.4.24) SDK
+
+#ifdef _MSC_VER
+	#pragma pack(push, 0x8)
+#endif
+
+namespace Classes
+{
+//---------------------------------------------------------------------------
+//Classes
+//---------------------------------------------------------------------------
+
+// WidgetBlueprintGeneratedClass BP_FppWeaponSlotHudWidget.BP_FppWeaponSlotHudWidget_C
+// 0x0000 (0x0278 - 0x0278)
+class UBP_FppWeaponSlotHudWidget_C : public UFppWeaponSlotHudBaseWidget
+{
+public:
+
+	static UClass* StaticClass()
+	{
+		static auto ptr = UObject::FindClass("WidgetBlueprintGeneratedClass BP_FppWeaponSlotHudWidget.BP_FppWeaponSlotHudWidget_C");
+		return ptr;
+	}
+
+};
+
+
+}
+
+#ifdef _MSC_VER
+	#pragma pack(pop)
+#endif
